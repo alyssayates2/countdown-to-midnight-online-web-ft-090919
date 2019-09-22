c822663ebc82bf3110827a4c519b51b803be4b97
Alyssa Yates <bosslys@icloud.com>
2019-09-22 18:54:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,14 @@
 def countdown(number)
   while number > 0
-    sleep 1 
+    sleep 1
     puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
-
-
+def countdown_with_sleep(number)
+end 
 
 
 
