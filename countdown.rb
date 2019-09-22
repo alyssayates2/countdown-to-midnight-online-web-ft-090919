8ef9e7fbc08254d65f99ce82a05d377eb62e3737
Alyssa Yates <bosslys@icloud.com>
2019-09-22 18:49:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,21 +1,43 @@
-def countdown(number)
-  while number > 0
-   sleep 1
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
+# def countdown(number)
+#   while number > 0
+#    sleep 1
+#     puts "#{number} SECOND(S)!"
+#     number -= 1
+#   end
+#   return "HAPPY NEW YEAR!"
+# end
+#
+# countdown(10)
+#
+# def countdown_with_sleep(number)
+#   while number > 0
+#    sleep 1
+#     puts "#{number} SECOND(S)!"
+#     number -= 1
+#   end
+#   return "HAPPY NEW YEAR!"
+# end
+#
+# countdown(10)
+#
 
-countdown(10)
 
-def countdown_with_sleep(number)
-  while number > 0
-   sleep 1
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
 
-countdown(10)
+
+# def using_until
+#   levitation_force = 6
+#   until levitation_force == 10
+#     puts "Wingardium Leviosa"
+#     levitation_force += 1
+#   end
+# end
+#
+#
+#
+# def using_while
+#   levitation_force = 6
+#   while levitation_force < 10
+#     puts "Wingardium Leviosa"
+#     levitation_force += 1
+#   end
+# end
